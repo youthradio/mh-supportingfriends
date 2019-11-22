5783fcddaddcef46a28b4bcfdb37205a28ca93df
shagia <usenet@shagia.me>
2019-11-22 15:56:13 -0800

    Add new BigImageContainer subcomponent

---
+++ components/Custom/BigImageContainer.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="bigImageContainer">
+    <div class="topLayer">
+      <div class="bigImage">
+        <img src="https://picsum.photos/600/300">
+      </div>
+      <div class="textContent">
+        Vitae purus faucibus ornare suspendisse sed nisi. Dignissim cras tincidunt lobortis feugiat vivamus at. Lorem mollis aliquam ut porttitor. Mauris in aliquam sem fringilla ut morbi tincidunt augue. Lorem ipsum dolor sit amet.
+        <br> <br>
+        Vitae purus faucibus ornare suspendisse sed nisi. Dignissim cras tincidunt lobortis feugiat vivamus at. Lorem mollis aliquam ut porttitor. Mauris in aliquam sem fringilla ut morbi tincidunt augue. Lorem ipsum dolor sit amet.
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    .bigImageContainer{
+    position: relative;
+    /* make container retain a size based on the text content*/
+    max-width: 100%;
+    max-height: 100%;
+    overflow: hidden;
+    }
+    .bigImage img{
+    z-index: -1;
+    position: absolute;
+    /* make image size based on the parent container, which is dependent on text */
+    height: 100%;
+    width: 100%;
+    /* clean stretching */
+    object-fit: cover;
+    object-position: center;
+    }
+    .textContent{
+    z-index: 1;
+    padding: 10px;
+    margin: 0 auto;
+    width: 300px;
+    height: 300px;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -11,19 +11,7 @@
       >
         {{ scenario }}
       </div>
-
-      <div class="bigImageContainer">
-        <div class="topLayer">
-          <div class="bigImage">
-            <img src="https://picsum.photos/600/300">
-          </div>
-          <div class="textContent">
-            Vitae purus faucibus ornare suspendisse sed nisi. Dignissim cras tincidunt lobortis feugiat vivamus at. Lorem mollis aliquam ut porttitor. Mauris in aliquam sem fringilla ut morbi tincidunt augue. Lorem ipsum dolor sit amet.
-            <br> <br>
-            Vitae purus faucibus ornare suspendisse sed nisi. Dignissim cras tincidunt lobortis feugiat vivamus at. Lorem mollis aliquam ut porttitor. Mauris in aliquam sem fringilla ut morbi tincidunt augue. Lorem ipsum dolor sit amet.
-          </div>
-        </div>
-      </div>
+      <ScenarioContainer />
     </article>
     <ShareContainer />
     <RelatedPostsContainer />
@@ -37,6 +25,7 @@ import CommonUtils from '../mixins/CommonUtils'
 import ArticleData from '../data/data.json'
 import RelatedPostsContainer from '~/components/RelatedPosts/RelatedPostsContainer'
 import HeaderContainer from '~/components/Header/HeaderContainer'
+import ScenarioContainer from '~/components/custom/ScenarioContainer'
 import ShareContainer from '~/components/custom/ShareContainer'
 import FooterContainer from '~/components/Footer/FooterContainer'
 
@@ -45,6 +34,7 @@ export default {
     RelatedPostsContainer,
     HeaderContainer,
     ShareContainer,
+    ScenarioContainer,
     FooterContainer
   },
   mixins: [
@@ -74,30 +64,4 @@ export default {
 <style lang="scss" scoped>
 @import "~@/css/vars";
 @import "~@/css/base";
-
-.bigImageContainer{
-  position: relative;
-  /* make container retain a size based on the text content*/
-  max-width: 100%;
-  max-height: 100%;
-  overflow: hidden;
-}
-.bigImage img{
-  z-index: -1;
-  position: absolute;
-  /* make image size based on the parent container, which is dependent on text */
-  height: 100%;
-  width: 100%;
-  /* clean stretching */
-  object-fit: cover;
-  object-position: center;
-}
-.textContent{
-  z-index: 1;
-  padding: 10px;
-  margin: 0 auto;
-  width: 300px;
-  height: 300px;
-}
-
 </style>
